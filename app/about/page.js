1729b095615e47bdf9e5ca7d6638661fe33c4640
zeromatchaberry <zeromatcha261@gmail.com>
2025-08-09 15:08:15 +0700

    fix about page error

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,7 @@ export default function AboutPage() {
     <main className="min-h-screen bg-gray-100 p-8">
       <h1 className="text-3xl font-bold mb-4">About Me</h1>
       <p className="text-gray-700">
-        Hi! I'm Sena, and this is my personal portfolio & blog website. I write about my projects, experiences, and random thoughts.
+        Hi! I&apos;m Sena, and this is my personal portfolio &amp; blog website. I write about my projects, experiences, and random thoughts.
       </p>
     </main>
   );
